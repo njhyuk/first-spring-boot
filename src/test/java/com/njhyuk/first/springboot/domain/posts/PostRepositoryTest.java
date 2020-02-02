0aeef16f95eb9ebdcbc448132cde465fff3212f5
JunHyuk Noh <junhyuk.noh@kurlycorp.com>
2020-02-02 18:19:21 +0900

    add post repository

---
+++ src/test/java/com/njhyuk/first/springboot/domain/posts/PostRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.njhyuk.first.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostRepositoryTest {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @After
+    public void cleanup() {
+        postRepository.deleteAll();
+    }
+
+    @Test
+    public void save_post_then_fetch() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("jojoldu@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
