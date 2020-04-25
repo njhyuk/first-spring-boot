27c96fdb3ec9a0c7c907b586460c4a2b21093c36
JunHyuk Noh <junhyuk.noh@kurlycorp.com>
2020-04-25 23:03:25 +0900

    LoginUser 어노테이션을 추가하여 메소드에서 반복되는 로직 sessionUser를 없앰

--- src/main/java/com/njhyuk/first/springboot/web/IndexController.java
+++ src/main/java/com/njhyuk/first/springboot/web/IndexController.java
@@ -1,6 +1,8 @@
 package com.njhyuk.first.springboot.web;
 
+import com.njhyuk.first.springboot.config.auth.LoginUser;
 import com.njhyuk.first.springboot.config.auth.dto.SessionUser;
+import com.njhyuk.first.springboot.domain.user.User;
 import com.njhyuk.first.springboot.service.posts.PostsService;
 import com.njhyuk.first.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,19 +10,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null) {
             model.addAttribute("userName", user.getName());
         }
