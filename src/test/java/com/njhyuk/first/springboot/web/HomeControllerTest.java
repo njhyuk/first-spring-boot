ed2ee399169cb7fe3d9c6e5efec7c026c1aff32f
JunHyuk Noh <junhyuk.noh@kurlycorp.com>
2020-04-26 01:33:52 +0900

    Oauth Mock 테스트 추가

--- src/test/java/com/njhyuk/first/springboot/web/HomeControllerTest.java
+++ src/test/java/com/njhyuk/first/springboot/web/HomeControllerTest.java
@@ -1,9 +1,13 @@
 package com.njhyuk.first.springboot.web;
 
+import com.njhyuk.first.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,13 +16,19 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest(controllers = HomeController.class, excludeFilters = {
+        @ComponentScan.Filter(
+                type = FilterType.ASSIGNABLE_TYPE,
+                classes = SecurityConfig.class
+        )
+})
 public class HomeControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
     @Test
+    @WithMockUser(roles = "USER")
     public void will_return_to_hello() throws Exception {
         String hello = "hello";
 
@@ -28,6 +38,7 @@ public class HomeControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     public void will_return_to_helloDto() throws Exception {
         String name = "hello";
         int amount = 1000;
